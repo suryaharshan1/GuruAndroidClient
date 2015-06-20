5a547b3a0fb9821466dc817bda42af2a2ba65c3d
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-20 18:28:06 +0530

    Completes GuruApiAuthenticator

--- app/src/main/java/com/windroilla/guru/authenticator/GuruApiAuthenticator.java
+++ app/src/main/java/com/windroilla/guru/authenticator/GuruApiAuthenticator.java
@@ -1,15 +1,21 @@
 package com.windroilla.guru.authenticator;
 
+import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
 import android.app.Application;
+import android.text.TextUtils;
 import android.util.Log;
 
 import com.squareup.okhttp.Authenticator;
+import com.squareup.okhttp.Challenge;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 import java.net.Proxy;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -34,7 +40,48 @@ public class GuruApiAuthenticator implements Authenticator{
     public Request authenticate(Proxy proxy, Response response) throws IOException {
         Log.d(TAG, "Authenticating for response: "  + response);
         Log.d(TAG, "Challenges: " + response.challenges());
-        return null;
+        String credential;
+        Log.d(TAG, "Request URL: " + response.request().url().getPath());
+        if (response.request().url().getPath().startsWith("/oauth2")) return response.request();
+
+        List<Challenge> challenges = response.challenges();
+
+        for (Challenge challenge : challenges) {
+            if (challenge.getScheme().equals("Bearer")) {
+                Account[] accounts = accountManager.getAccountsByType(AuthConstants.ACCOUNT_TYPE);
+                if (accounts.length != 0) {
+                    String oldToken = accountManager.peekAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE);
+                    if (!TextUtils.isEmpty(oldToken)) {
+                        Log.d(TAG, "Invalidating auth token");
+                        accountManager.invalidateAuthToken(AuthConstants.ACCOUNT_TYPE, oldToken);
+                    }
+                    try {
+                        Log.d(TAG, "Calling accountManager.blockingGetAuthToken");
+                        String token = accountManager.blockingGetAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE, false);
+
+                        if (token == null) {
+                            accountManager.removeAccount(accounts[0], null, null);
+                        }
+
+                        // Do not retry certain URLs
+                        //if (url.getPath().startsWith("/donotretry")) {
+                        //  return null;
+                        //} else if (token != null) {
+                        if (token != null) {
+                            return response.request().newBuilder().header("Authorization", "Bearer " + token).build();
+                        }
+
+                    } catch (AuthenticatorException e) {
+                        Log.e(TAG, "Authenticator Exception");
+                        e.printStackTrace();
+                    } catch (OperationCanceledException e) {
+                        Log.e(TAG, "Operation Canceled Exception");
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return response.request();
     }
 
     @Override
