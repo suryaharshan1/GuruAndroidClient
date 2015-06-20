b815ea939706ffaa0679d6690050382a1de2a4e2
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-21 00:27:51 +0530

    Fix Dagger Library

--- app/src/main/java/com/windroilla/guru/GuruApplication.java
+++ app/src/main/java/com/windroilla/guru/GuruApplication.java
@@ -6,5 +6,22 @@ import android.app.Application;
  * Created by Surya Harsha Nunnaguppala on 20/6/15.
  */
 public class GuruApplication extends Application {
+    private static GuruGraph graph;
+    private static GuruApplication instance;
 
+    public static GuruGraph component() {
+        return graph;
+    }
+
+    //TODO Complete implementation after GuruComponent
+    public static void buildComponentAndInject() {
+        //graph = GuruComponent.Initializer.init(instance);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+        buildComponentAndInject();
+    }
 }
