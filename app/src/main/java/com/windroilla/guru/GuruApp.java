c0879a7b3cd4629620b90d90f9b73af752dc8ca1
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-24 01:56:45 +0530

    Recreate GuruApp and GuruGraph

---
+++ app/src/main/java/com/windroilla/guru/GuruApp.java
@@ -0,0 +1,32 @@
+package com.windroilla.guru;
+
+import android.app.Application;
+
+import com.windroilla.guru.authenticator.ApiModule;
+
+/**
+ * Created by Surya Harsha Nunnaguppala on 23/6/15.
+ */
+public class GuruApp extends Application {
+    private static GuruApp sInstance;
+    private GuruGraph guruGraph;
+
+    public static GuruApp getsInstance() {
+        return sInstance;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sInstance = this;
+        guruGraph = DaggerGuruGraph.builder()
+                .apiModule(new ApiModule(this))
+                .build();
+    }
+
+    public GuruGraph graph() {
+        return guruGraph;
+    }
+
+}
---
+++ app/src/main/java/com/windroilla/guru/GuruGraph.java
@@ -0,0 +1,25 @@
+package com.windroilla.guru;
+
+import com.windroilla.guru.authenticator.ApiHeaders;
+import com.windroilla.guru.authenticator.ApiModule;
+import com.windroilla.guru.authenticator.GuruAuthenticatorActivity;
+
+import javax.inject.Singleton;
+
+import dagger.Component;
+
+/**
+ * Created by Surya Harsha Nunnaguppala on 23/6/15.
+ */
+@Singleton
+@Component(modules = {ApiModule.class})
+
+public interface GuruGraph {
+
+    void inject(HelperActivity activity);
+
+    void inject(GuruAuthenticatorActivity activity);
+
+    void inject(ApiHeaders apiHeaders);
+
+}
