16857d8d32bdee975e60bff2aa7c3fa8690e2ffc
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-24 16:35:21 +0530

    Update RequestAccessTokenByPassword construct with client_id and client_secret

--- app/src/main/java/com/windroilla/guru/authenticator/RequestAccessTokenByPassword.java
+++ app/src/main/java/com/windroilla/guru/authenticator/RequestAccessTokenByPassword.java
@@ -1,20 +1,19 @@
 package com.windroilla.guru.authenticator;
 
-import javax.inject.Inject;
-import javax.inject.Named;
-
 /**
  * Created by Surya Harsha Nunnaguppala on 19/6/15.
  */
 public class RequestAccessTokenByPassword {
-    public static final String grant_type = "password";
+    public final String grant_type = "password";
     public final String username;
     public final String password;
-    @Inject @Named("ClientId") String client_id;
-    @Inject @Named("ClientSecret") String client_secret;
+    public final String client_id;
+    public final String client_secret;
 
-    public RequestAccessTokenByPassword(String username, String password) {
+    public RequestAccessTokenByPassword(String username, String password, String client_id, String client_secret) {
         this.username = username;
         this.password = password;
+        this.client_id = client_id;
+        this.client_secret = client_secret;
     }
 }
