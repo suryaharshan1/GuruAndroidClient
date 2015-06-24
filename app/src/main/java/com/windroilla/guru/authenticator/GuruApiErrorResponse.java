dfc15808d5f8e5fb90feb332d7af2f5e6ac81ff8
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-24 13:44:25 +0530

    Create GuruApiErrorResponse class

---
+++ app/src/main/java/com/windroilla/guru/authenticator/GuruApiErrorResponse.java
@@ -0,0 +1,12 @@
+package com.windroilla.guru.authenticator;
+
+/**
+ * Created by Surya Harsha Nunnaguppala on 24/6/15.
+ */
+public class GuruApiErrorResponse {
+    public String name;
+    public String message;
+    public int code;
+    public int status;
+    public String type;
+}
