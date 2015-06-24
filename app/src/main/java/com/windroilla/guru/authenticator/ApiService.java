342497dd9ba0ae253e379bb7043fefb90ac78204
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-24 16:36:30 +0530

    Add static headers to the requests in ApiService

--- app/src/main/java/com/windroilla/guru/authenticator/ApiService.java
+++ app/src/main/java/com/windroilla/guru/authenticator/ApiService.java
@@ -1,6 +1,7 @@
 package com.windroilla.guru.authenticator;
 
 import retrofit.http.Body;
+import retrofit.http.Headers;
 import retrofit.http.POST;
 import rx.Observable;
 
@@ -8,9 +9,12 @@ import rx.Observable;
  * Created by Surya Harsha Nunnaguppala on 19/6/15.
  */
 public interface ApiService {
+    @Headers({"Content-Type:application/json-patch+json"})
     @POST("/oauth2/token") AccessToken getAccessToken(@Body RequestAccessTokenByPassword body);
 
+    @Headers({"Content-Type:application/json-patch+json"})
     @POST("/oauth2/token") AccessToken refreshAccessToken(@Body RequestAccessTokenByRefresh body);
 
+    @Headers({"Content-Type:application/json-patch+json"})
     @POST("/oauth2/token") Observable<AccessToken> getAccessTokenObservable(@Body RequestAccessTokenByPassword body);
 }
