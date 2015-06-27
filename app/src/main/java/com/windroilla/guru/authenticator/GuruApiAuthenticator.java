5a1f53a4f948c1a185bf5b6dceeb725c0ded8508
Surya Harsha Nunnaguppala <suryaharshan1@gmail.com>
2015-06-27 22:57:52 +0530

    Fix GuruApiAuthenticator to refresh token on response status 401

--- app/src/main/java/com/windroilla/guru/authenticator/GuruApiAuthenticator.java
+++ app/src/main/java/com/windroilla/guru/authenticator/GuruApiAuthenticator.java
@@ -9,13 +9,11 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.squareup.okhttp.Authenticator;
-import com.squareup.okhttp.Challenge;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
 import java.io.IOException;
 import java.net.Proxy;
-import java.util.List;
 
 import javax.inject.Inject;
 
@@ -42,46 +40,40 @@ public class GuruApiAuthenticator implements Authenticator{
         Log.d(TAG, "Challenges: " + response.challenges());
         String credential;
         Log.d(TAG, "Request URL: " + response.request().url().getPath());
-        if (response.request().url().getPath().startsWith("/oauth2")) return response.request();
+        if (response.request().url().getPath().startsWith("/guru/oauth2")) return null;
 
-        List<Challenge> challenges = response.challenges();
+        Account[] accounts = accountManager.getAccountsByType(AuthConstants.ACCOUNT_TYPE);
+        if (accounts.length != 0) {
+            String oldToken = accountManager.peekAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE);
+            if (!TextUtils.isEmpty(oldToken)) {
+                Log.d(TAG, "Invalidating auth token");
+                accountManager.invalidateAuthToken(AuthConstants.ACCOUNT_TYPE, oldToken);
+            }
+            try {
+                Log.d(TAG, "Calling accountManager.blockingGetAuthToken");
+                String token = accountManager.blockingGetAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE, false);
 
-        for (Challenge challenge : challenges) {
-            if (challenge.getScheme().equals("Bearer")) {
-                Account[] accounts = accountManager.getAccountsByType(AuthConstants.ACCOUNT_TYPE);
-                if (accounts.length != 0) {
-                    String oldToken = accountManager.peekAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE);
-                    if (!TextUtils.isEmpty(oldToken)) {
-                        Log.d(TAG, "Invalidating auth token");
-                        accountManager.invalidateAuthToken(AuthConstants.ACCOUNT_TYPE, oldToken);
-                    }
-                    try {
-                        Log.d(TAG, "Calling accountManager.blockingGetAuthToken");
-                        String token = accountManager.blockingGetAuthToken(accounts[0], AuthConstants.AUTHTOKEN_TYPE, false);
-
-                        if (token == null) {
-                            accountManager.removeAccount(accounts[0], null, null);
-                        }
-
-                        // Do not retry certain URLs
-                        //if (url.getPath().startsWith("/donotretry")) {
-                        //  return null;
-                        //} else if (token != null) {
-                        if (token != null) {
-                            return response.request().newBuilder().header("Authorization", "Bearer " + token).build();
-                        }
-
-                    } catch (AuthenticatorException e) {
-                        Log.e(TAG, "Authenticator Exception");
-                        e.printStackTrace();
-                    } catch (OperationCanceledException e) {
-                        Log.e(TAG, "Operation Canceled Exception");
-                        e.printStackTrace();
-                    }
+                if (token == null) {
+                    accountManager.removeAccount(accounts[0], null, null);
                 }
+
+                // Do not retry certain URLs
+                //if (url.getPath().startsWith("/donotretry")) {
+                //  return null;
+                //} else if (token != null) {
+                if (token != null && !response.request().header("Authorization").equals("Bearer " + token)) {
+                    return response.request().newBuilder().header("Authorization", "Bearer " + token).build();
+                }
+
+            } catch (AuthenticatorException e) {
+                Log.e(TAG, "Authenticator Exception");
+                e.printStackTrace();
+            } catch (OperationCanceledException e) {
+                Log.e(TAG, "Operation Canceled Exception");
+                e.printStackTrace();
             }
         }
-        return response.request();
+        return null;
     }
 
     @Override
